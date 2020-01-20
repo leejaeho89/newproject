6d0fee4dff38bd1d1a0bafc6efcb9e84ed801fc9
leejaeho89 <woghgo@naver.com>
2020-01-20 19:20:08 +0900

    3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React, {component} from 'react';
-import Customer from './component/Customer'
+import React, { component } from 'react';
+import Customer from './components/Customer'
 import './App.css';
 
 const customer = {
-'name': '홍길동',
-'birthday': '961222',
-'gender': '남자',
-'job': '대학생'
-}
-
-class App extends component {
-render() {
-return (
-        <Customer
-        name={customer.name}
-        birthday={customer.birthday}
-        gender={customer.gender}
-        job={customer.job}
-/>
-);
-}
-}
-
-export default App;
+    'name': '홍길동',
+    'birthday': '961222',
+    'gender': '남자',
+    'job': '대학생'
+    }
+    
+    class App extends component {
+    render() {
+    return (
+    <Customer
+    name={customer.name}
+    birthday={customer.birthday}
+    gender={customer.gender}
+    job={customer.job}
+    />
+    );
+    }
+    }
+    
+    export default App;
+    
